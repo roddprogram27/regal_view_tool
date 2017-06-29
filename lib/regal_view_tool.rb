78c54587e80aad3781e94d50201960173c3623aa
Jordan Hudgens <jordan@devcamp.com>
2017-06-29 14:56:43 +0000

    Dev tool initial commit

---
+++ lib/regal_view_tool.rb
@@ -0,0 +1,6 @@
+require "regal_view_tool/version"
+require "regal_view_tool/renderer"
+
+module RegalViewTool
+  # Your code goes here...
+end
---
+++ lib/regal_view_tool/version.rb
@@ -0,0 +1,3 @@
+module RegalViewTool
+  VERSION = "0.1.0"
+end
